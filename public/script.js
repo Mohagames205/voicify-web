05f74e84e6b3bb33f9c9d7af4829dd334a81584c
Mohamed <mohamedelyousfi87@gmail.com>
2021-05-22 23:24:07 +0200

    First ever commit :o

---
+++ public/script.js
@@ -0,0 +1,77 @@
+const socket = io('/')
+const audioGrid = document.getElementById('audio-grid')
+const userElement = document.getElementById("user-list")
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+})
+const myAudio = document.createElement('audio')
+myAudio.muted = true
+const peers = {}
+
+var username;
+
+navigator.mediaDevices.getUserMedia({
+  video: false,
+  audio: true
+}).then(stream => {
+  addAudioStream(myAudio, stream)
+
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const audio = document.createElement('audio')
+    call.on('stream', userAudioStream => {
+      addAudioStream(audio, userAudioStream)
+    })
+  })
+
+  socket.on('user-connected', (userId) => {
+    setTimeout(connectToNewUser,1000,userId,stream)
+  })
+})
+
+socket.on("user-list-update", (userList) => {
+  userElement.innerHTML = '';
+  for(const user of userList){
+    var li = document.createElement("li");
+
+    if(username == user){
+      li.style.color = "green";
+      li.style.fontWeight = "bold";
+    }
+
+    li.appendChild(document.createTextNode(user));
+    
+    userElement.appendChild(li);
+  }
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  username = id;
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const audio = document.createElement('audio')
+  call.on('stream', userAudioStream => {
+    addAudioStream(audio, userAudioStream)
+  })
+  call.on('close', () => {
+    audio.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addAudioStream(audio, stream) {
+  audio.srcObject = stream
+  audio.addEventListener('loadedmetadata', () => {
+    audio.play()
+  })
+  audioGrid.append(audio)
+}
---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room})
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+
+    
+
+    socket.join(roomId)
+    socket.to(roomId).emit('user-connected', userId)
+    socket.username = userId
+    socket.roomId = roomId
+
+    clientNames = []
+
+    io.in(roomId).fetchSockets().then(clients => {
+      for(const client of clients){
+          clientNames.push(client.username)
+      }
+
+      socket.to(roomId).emit('user-list-update', clientNames);
+      io.sockets.in(roomId).emit('user-list-update', clientNames);
+    });
+
+    socket.on('disconnect', () => {
+      clientNames = []
+
+    io.in(roomId).fetchSockets().then(clients => {
+      for(const client of clients){
+          clientNames.push(client.username)
+      }
+
+      socket.to(roomId).emit('user-list-update', clientNames);
+      io.to(roomId).emit('user-list-update', clientNames);
+    });
+
+      socket.to(roomId).emit('user-disconnected', userId)
+    })
+
+  })
+})
+
+server.listen(80, '0.0.0.0')
